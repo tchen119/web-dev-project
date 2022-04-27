0541db039a83b5453a9065ef557e325584dc32cf
Tina C <tinac@Tinas-MacBook-Air.local>
2022-04-27 19:22:25 -0400

    local link

--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,1 @@
-export const API_BASE = 'https://web-dev-restaurant.herokuapp.com'; // 'http://localhost:4000'
+export const API_BASE = 'http://localhost:4000' //'https://web-dev-restaurant.herokuapp.com';
